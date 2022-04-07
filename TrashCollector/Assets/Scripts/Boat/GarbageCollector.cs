09d4bbfb85277d543706527a5c4547383c1a43ef
Udith Eranga <86252373+UdithPremarathne@users.noreply.github.com>
2022-04-07 16:06:10 +0530

    Garbage Collection
    Garbage Collection completed

---
+++ TrashCollector/Assets/Scripts/Boat/GarbageCollector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageCollector : MonoBehaviour
+{
+    private int garbage = 0;
+    public int garbageLimit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Garbage"))
+        {
+            if (garbage < garbageLimit)
+            {
+                Destroy(collision.gameObject);
+                garbage++;
+                Debug.Log("Garbage count : " + garbage);
+            }
+            else
+            {
+                Debug.Log("Garbage Full");
+            }
+            
+        }
+        
+    }
+}
